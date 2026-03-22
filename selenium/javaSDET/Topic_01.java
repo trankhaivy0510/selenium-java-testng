db7a9391770aa2d640c544e5e2a8a9956551f54b
Tran Khai Vy <khaivy.tran@finviet.corp>
2026-03-22 14:19:11 +0700

    Initial commit

---
+++ selenium/javaSDET/Topic_01.java
@@ -0,0 +1,27 @@
+package javaSDET;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Topic_01 {
+    public static void main(String[] args) {
+        String browser = "firefox"; // đổi thành firefox / edge
+
+        WebDriver driver;
+
+        switch (browser.toLowerCase()) {
+            case "firefox":
+                driver = new FirefoxDriver();
+                break;
+            case "edge":
+                driver = new EdgeDriver();
+                break;
+            default:
+                driver = new ChromeDriver();
+        }
+
+        driver.get("https://google.com");
+    }
+}
