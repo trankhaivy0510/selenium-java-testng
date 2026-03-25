7a678998f595178ed40a704bbbf257dacaaee88d
khaivy.tran <trankhaivy0510@gmail.com>
2026-03-25 20:54:04 +0700

    add topic 2

--- selenium/javaSDET/Topic_2.java
+++ selenium/javaSDET/Topic_2.java
@@ -2,6 +2,6 @@ package javaSDET;
 
 public class Topic_2 {
     public static void main(String[] args) {
-        System.out.println("Helloworld");
+        System.out.println("Helloworld ABC");
     }
 }
