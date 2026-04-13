0e4ed82091ebb67e30bf29042cf22bd846ed87a6
khaivy.tran <trankhaivy0510@gmail.com>
2026-04-13 21:53:48 +0700

    add Relative_Locator func

--- selenium/javaSDET/Topic_2.java
+++ selenium/javaSDET/Topic_2.java
@@ -1,7 +1,58 @@
 package javaSDET;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.locators.RelativeLocator;
+import org.testng.annotations.*;
+
 public class Topic_2 {
-    public static void main(String[] args) {
-        System.out.println("Helloworld ABC");
+    WebDriver driver;
+
+    @BeforeTest
+    public void initialBrowser() {
+        driver = new ChromeDriver();
+        driver.get("https://demo.nopcommerce.com/login?returnUrl=%2F");
+        WebElement emailField = driver.findElement(By.id("Email"));
+        emailField.sendKeys("");
+    }
+
+    @Test
+    public void Register(){
+        driver = new ChromeDriver();
+        driver.get("https://www.nopcommerce.com/vi/register?returnUrl=%2Fvi%2Fget-started");
+        WebElement lastNameField = driver.findElement(By.id("LastName"));
+        lastNameField.click();
+        lastNameField.sendKeys("Tran");
+    }
+
+
+    @Test
+    public void Login(){
+
+    }
+
+    @Test
+    public void TCS03_Relative_Locator (){
+        WebElement rememberMeCheckbox = driver.findElement(By.cssSelector("input#RememberMe"));
+        WebElement forgotPasswordLinkBy = driver.findElement(By.cssSelector("a[href='/passwordrecovery']"));
+        WebElement loginButton = driver.findElement(By.cssSelector("button.login-button"));
+        WebElement passwordField = driver.findElement(By.cssSelector("input#Password"));
+
+        WebElement rememberMeLabel = driver.findElement(RelativeLocator.with(By.tagName("label"))
+                .above(loginButton)
+                .below(passwordField)
+                .toRightOf(rememberMeCheckbox)
+                .toLeftOf(forgotPasswordLinkBy)
+        );
+
+//        System.out.println(rememberMeLabel.toString());
+
+    }
+
+    @AfterClass
+    public void closeBrowser(){
+        driver.quit();
     }
 }
