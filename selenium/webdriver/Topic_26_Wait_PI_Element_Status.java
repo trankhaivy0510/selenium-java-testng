c5cdfa6dcb0650da5c195d4c4138e344bedce432
Tran Khai Vy <trankhaivy0510@gmail.com>
2026-09-02 21:05:32 +0700

    update topic upload file

---
+++ selenium/webdriver/Topic_26_Wait_PI_Element_Status.java
@@ -0,0 +1,80 @@
+package webdriver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class Topic_26_Wait_PI_Element_Status {
+    WebDriver driver;
+    WebDriverWait driverWait;
+
+    @BeforeClass
+    public void initBrowser(){
+        driver = new ChromeDriver();
+        driver.manage().window().fullscreen();
+
+        driverWait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+
+    @Test
+    public void TC_01_Visible(){
+        driver.get("https://live.techpanda.org/index.php/customer/account/login/");
+        driver.findElement(By.cssSelector("button#send2")).click();
+
+        // Điều kiện 1 element xuất hiện trên UI và trong HTML
+        driverWait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div#advice-required-entry-email")));
+
+        Assert.assertTrue(driver.findElement(By.cssSelector("div#advice-required-entry-email")).isDisplayed());
+
+
+    }
+
+    @Test
+    public void TC_02_InvisibleI_Element_Found(){
+        driver.get("https://live.techpanda.org/index.php/customer/account/login/");
+        driver.findElement(By.cssSelector("button#send2")).click();
+
+        driver.findElement(By.cssSelector("ul.form-list input[type='email']")).sendKeys("vy@gmail.com");
+        driver.findElement(By.cssSelector("button#send2")).click();
+
+        // Điều kiện 2 element không có trên UI nhưng có trong HTML
+        // Email address error mesage không có trên Ui nhưng vẫn còn trong HTML
+        driverWait.until(ExpectedConditions.invisibilityOf(driver.findElement(By.cssSelector("div#advice-required-entry-email"))));
+        Assert.assertFalse(driver.findElement(By.cssSelector("div#advice-required-entry-email")).isDisplayed());
+    }
+
+    @Test
+    public void TC_03_InvisibleII_Element_Found(){
+        driver.get("https://live.techpanda.org/index.php/customer/account/login/");
+        driver.findElement(By.cssSelector("button#send2")).click();
+
+        // Điều kiện 1 element xuất hiện trên UI và trong HTML
+        driverWait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div#advice-required-entry-email")));
+
+        Assert.assertTrue(driver.findElement(By.cssSelector("div#advice-required-entry-email")).isDisplayed());
+        driver.findElement(By.cssSelector("ul.form-list input[type='email']")).sendKeys("vy@gmail.com");
+        driver.findElement(By.cssSelector("ul.form-list input[type='password']")).sendKeys("123456");
+        driver.findElement(By.cssSelector("button#send2")).click();
+        driverWait.until(ExpectedConditions.invisibilityOf(driver.findElement(By.cssSelector("div#advice-required-entry-email"))));
+        Assert.assertFalse(driver.findElement(By.cssSelector("div#advice-required-entry-email")).isDisplayed());
+    }
+
+    @Test
+    public void TC_03_Presence(){
+
+    }
+
+    @Test
+    public void TC_04_Stateness(){
+
+    }
+}
