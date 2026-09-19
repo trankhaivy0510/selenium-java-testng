16d64efaa7be60966cadf73ccee73120c5edf954
trankhaivy <trankhaivy0510@gmail.com>
2026-09-19 20:48:00 +0700

    wait part one

---
+++ selenium/webdriver/Topic_26_Wait_PII_FindElement.java
@@ -0,0 +1,4 @@
+package webdriver;
+
+public class Topic_26_Wait_PII_FindElement {
+}
--- selenium/webdriver/Topic_26_Wait_PI_Element_Status.java
+++ selenium/webdriver/Topic_26_Wait_PI_Element_Status.java
@@ -2,6 +2,7 @@ package webdriver;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -22,6 +23,7 @@ public class Topic_26_Wait_PI_Element_Status {
         driver.manage().window().fullscreen();
 
         driverWait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(12));
     }
 
 
@@ -53,28 +55,63 @@ public class Topic_26_Wait_PI_Element_Status {
     }
 
     @Test
-    public void TC_03_InvisibleII_Element_Found(){
+    public void TC_03_InvisibleII_Element_NotFound(){
         driver.get("https://live.techpanda.org/index.php/customer/account/login/");
-        driver.findElement(By.cssSelector("button#send2")).click();
 
-        // Điều kiện 1 element xuất hiện trên UI và trong HTML
-        driverWait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div#advice-required-entry-email")));
+        //Điều kiện 2+3: Elemnet ko có trong UI và cũng không có trong HTML
+        // Email Address error message không có trên UI và không còn trong HTML
 
-        Assert.assertTrue(driver.findElement(By.cssSelector("div#advice-required-entry-email")).isDisplayed());
-        driver.findElement(By.cssSelector("ul.form-list input[type='email']")).sendKeys("vy@gmail.com");
-        driver.findElement(By.cssSelector("ul.form-list input[type='password']")).sendKeys("123456");
-        driver.findElement(By.cssSelector("button#send2")).click();
+//        driver.findElement(By.cssSelector("div#advice-required-entry-email"));
+
+        // Step này chạy lâu vì cầng element (findElement) mà element lại không có trong HTML
+        // Chờ và tìm đi tìm lại cho đến khi hết timeout
+
+        // Thứ tự khi có cả implicit với WebDriverWait:
+        // 1: Ta xét findElement trước -> wait 12s
+        // 2. Sau đó mới invisibilityOf(..) -> wait 10s
         driverWait.until(ExpectedConditions.invisibilityOf(driver.findElement(By.cssSelector("div#advice-required-entry-email"))));
-        Assert.assertFalse(driver.findElement(By.cssSelector("div#advice-required-entry-email")).isDisplayed());
+
+//        Assert.assertFalse(driver.findElement(By.cssSelector("div#advice-required-entry-email")).isDisplayed());
     }
 
     @Test
     public void TC_03_Presence(){
+        // Điều kiện 1: Element có trên UI và có trong HTML
+        driver.get("https://live.techpanda.org/index.php/customer/account/login/");
+
+        driver.findElement(By.cssSelector("button#send2")).click();
+        driverWait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("div#advice-required-entry-email")));
+
+        // Điều kiện 2: Element không có trên UI nhưng có trong HTML
+        driver.findElement(By.cssSelector("ul.form-list input[type='email']")).sendKeys("vy@gmail.com");
+        driver.findElement(By.cssSelector("button#send2")).click();
+        driverWait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("div#advice-required-entry-email")));
+
+        // Miễn sao có xuất hiện trong HTML là presence
+    }
+
+    @Test
+    public void TC_04_Staleness(){
+        // Tại thời điểm A element xuất hiện = lưu element
+        // Tại thời điểm B element không xuất hiện trong HTML nữa = dùng element đã lưu tại thời điểm A check
+        // element đó staleness tại thời điểm B
+
+        driver.get("https://live.techpanda.org/index.php/customer/account/login/");
+        driver.findElement(By.cssSelector("button#send2")).click();
+
+        WebElement emailErrorMessage = driver.findElement(By.cssSelector("div#advice-required-entry-email"));
+
+        driver.findElement(By.cssSelector("ul.form-list input[type='email']")).sendKeys("vy@gmail.com");
+        driver.findElement(By.cssSelector("ul.form-list input[type='password']")).sendKeys("123456");
+        driver.findElement(By.cssSelector("button#send2")).click();
+
+        
+        driverWait.until(ExpectedConditions.stalenessOf(emailErrorMessage));
 
     }
 
     @Test
-    public void TC_04_Stateness(){
+    public void TC_05_(){
 
     }
 }
